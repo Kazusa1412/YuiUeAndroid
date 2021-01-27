aa4c4b2261db8f74ac32ed1a09caba2cbb87765f
kazusa1412 <549324884@qq.com>
2021-01-27 17:16:09 +0800

    .

---
+++ app/src/main/java/com/elouyi/yuiue/yw/component/YwDialog.kt
@@ -0,0 +1,10 @@
+package com.elouyi.yuiue.yw.component
+
+import android.app.Dialog
+import android.content.Context
+
+class YwDialog(context: Context, themeResId: Int) : Dialog(context, themeResId) {
+
+    constructor(context: Context): this(context,0)
+
+}
